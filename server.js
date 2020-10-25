94f67022f41dcbb0faca70d78377a14dbee0181a
Dar Dualba <ddualba@gmail.com>
2020-10-24 21:13:16 -0700

    initial commit, add server.js and basic route

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const placesRoutes = require('./routes/places-routes');
+
+// Init Middleware (BodyParser now built into express)
+app.use(express.json({ extended: false }));
+
+const PORT = process.env.PORT || 5000;
+
+//Define Routes
+app.use(placesRoutes);
+// app.use('/api/users', require('./routes/api/users'));
+
+app.listen(PORT, () => console.log(`Server started on Port ${PORT}`));
