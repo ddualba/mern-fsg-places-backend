f6e534f1633851fff44215e44d6145dd5380e6c6
Dar Dualba <ddualba@gmail.com>
2020-10-26 12:55:17 -0700

    add error handling for unsupported routes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 const placesRoutes = require('./routes/places-routes');
+const HttpError = require('./models/http-error');
 
 // Init Middleware (BodyParser now built into express)
 app.use(express.json({ extended: false }));
@@ -12,6 +13,13 @@ const PORT = process.env.PORT || 5000;
 app.use('/api/places', placesRoutes);
 // app.use('/api/users', require('./routes/api/users'));
 
+// Handle errors on unsupported routes
+app.use((req, res, next) => {
+  const error = new HttpError('Could not find this route.', 404);
+  throw error;
+});
+
+// error handling middleware
 app.use((error, req, res, next) => {
   if (res.headerSent) {
     return next(error);
