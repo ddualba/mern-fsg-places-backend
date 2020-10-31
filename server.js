9b9992728321b94a0d1f2da5fdee0f1d913c97d4
Dar Dualba <ddualba@gmail.com>
2020-10-30 23:06:15 -0700

    add CORS headers middleware to server.js

--- server.js
+++ server.js
@@ -15,6 +15,18 @@ app.use(express.json({ extended: false }));
 
 const PORT = process.env.PORT || 5500;
 
+// CORS Middleware
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+  next();
+});
+
 //Define Routes
 app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
